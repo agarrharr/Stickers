d130b0dc33ef842e9f06b6f61817c9801d6afa72
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2024-02-28 15:34:12 -0700

    Initial Commit

---
+++ Stickers/StickersApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickersApp.swift
+//  Stickers
+//
+//  Created by Adam Garrett-Harris on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct StickersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
