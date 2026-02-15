79d6786a2a631af1fcbf707ee0f6ffc49d36e799
Adam Garrett-Harris <3266363+agarrharr@users.noreply.github.com>
2026-02-15 10:51:09 -0700

    Add createdAt back

--- Modules/Sources/Models/Sticker.swift
+++ Modules/Sources/Models/Sticker.swift
@@ -6,10 +6,12 @@ public struct Sticker: Identifiable, Equatable, Sendable {
     public let id: UUID
     public var chartID: Chart.ID
     public var imageName = ""
+    public var createdAt: Date = Date()
 
-    public init(id: UUID, chartID: Chart.ID, imageName: String = "") {
+    public init(id: UUID, chartID: Chart.ID, imageName: String = "", createdAt: Date = Date()) {
         self.id = id
         self.chartID = chartID
         self.imageName = imageName
+        self.createdAt = createdAt
     }
 }
